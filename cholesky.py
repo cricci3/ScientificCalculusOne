4538be155b69e30f3424513a2719dd1d499a8292
cricci3 <claudioricci.99@gmail.com>
2024-06-26 16:33:28 +0200

    cholesky method but error too big

--- cholesky.py
+++ cholesky.py
@@ -1,48 +1,69 @@
 import scipy.io
 import numpy as np
-from scipy.linalg import cholesky, LinAlgError
-import scipy.sparse.linalg as spla
+from scipy.sparse import csc_matrix
+from scipy.sparse.linalg import splu, spsolve, eigsh
+from numpy.linalg import norm
 
 
-#  Matrix should be symmetric and positive definite
-#  Definita positiva se
-#       tutti autovalori > 0
-#       elementi su diagonale > 0
-
-
+# Funzione per verificare se la matrice è simmetrica
 def is_symmetric(matrix):
     return np.all(matrix.data == matrix.transpose().data)
 
 
-#  def is_positive_definite(matrix):
-#      return np.all(np.linalg.eigvals(matrix.toarray())) > 0 and np.all(np.diag(matrix)) > 0
-
+# Funzione per verificare se la matrice è definita positiva
 def is_positive_definite(matrix):
     try:
-        eigenvalue, _ = spla.eigsh(matrix, k=1, which='LM')
+        eigenvalue, _ = eigsh(matrix, k=1, which='LM')
         return np.all(matrix.diagonal() > 0) and eigenvalue > 0
-    except spla.ArpackNoConvergence:
+    except:
         return False
 
 
-# Load the .mat file
-data = scipy.io.loadmat('Matrix/cfd1.mat')
+# Funzione per risolvere il sistema Ax = b usando la decomposizione LU
+def solution(matrix):
+    n = matrix.shape[0]
+    xe = np.ones(n)
 
-# Stampare le chiavi per esaminare la struttura del dizionario
-print(data.keys())
+    # Calcolo del termine noto b
+    b = matrix @ xe
 
-A = data['Problem'][0, 0]['A']
-A = scipy.sparse.csr_matrix(A)
+    # Converte la matrice in formato CSC
+    matrix_csc = csc_matrix(matrix)
 
-# Verifica se la matrice è simmetrica
-if is_symmetric(A):
-    print("La matrice è simmetrica.")
-else:
-    print("La matrice non è simmetrica.")
+    # Decomposizione di LU della matrice
+    lu = splu(matrix_csc)
 
-# Verifica se la matrice è definita positiva
-if is_positive_definite(A):
-    print("La matrice è definita positiva.")
-else:
-    print("La matrice non è definita positiva.")
+    # Risoluzione del sistema Ax = b
+    # Risolviamo per y nel sistema triangolare inferiore L * y = b
+    y = spsolve(lu.L, b)
 
+    # Risolviamo per x nel sistema triangolare superiore U * x = y
+    x = spsolve(lu.U, y)
+
+    # Verifica dell'errore
+    errore_relativo = norm(x - xe, 2) / norm(xe, 2)
+    print(f"Errore relativo: {errore_relativo}")
+
+
+if __name__ == '__main__':
+    # Carica il file .mat
+    data = scipy.io.loadmat('Matrix/cfd1.mat')
+
+    # Stampare le chiavi per esaminare la struttura del dizionario
+    print(data.keys())
+
+    A = data['Problem'][0, 0]['A']
+    A = csc_matrix(A)
+
+    # Verifica se la matrice è simmetrica
+    if is_symmetric(A):
+        print("La matrice è simmetrica.")
+        # Verifica se la matrice è definita positiva
+        if is_positive_definite(A):
+            print("La matrice è definita positiva.")
+        else:
+            print("La matrice non è definita positiva.")
+    else:
+        print("La matrice non è simmetrica.")
+
+    solution(A)
