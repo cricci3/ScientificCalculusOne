0df8f82a5f3ad21c9994fd09c0ee19befbd121ab
dami013 <damianoficara@gmail.com>
2024-07-01 11:56:39 +0200

    convert 0.0 to null

--- Calculus_Java/untitled/src/main/java/org/example/JSONWriter.java
+++ Calculus_Java/untitled/src/main/java/org/example/JSONWriter.java
@@ -49,9 +49,26 @@ public class JSONWriter {
         for (MatrixData data : dataList) {
             ObjectNode matrixNode = matrixResults.addObject();
             matrixNode.put("File", data.getMatrixName());
-            matrixNode.put("Errore_Relativo", data.getErroreRelativo());
-            matrixNode.put("Time", data.getTime());
-            matrixNode.put("Memory_Used", (data.getMemoryUsed() / 1024.0) / 1024.0); // Convert to MB
+
+            // Use null for 0.0 values
+            if (data.getErroreRelativo() == 0.0) {
+                matrixNode.putNull("Errore_Relativo");
+            } else {
+                matrixNode.put("Errore_Relativo", data.getErroreRelativo());
+            }
+
+            if (data.getTime() == 0.0) {
+                matrixNode.putNull("Time");
+            } else {
+                matrixNode.put("Time", data.getTime());
+            }
+
+            if (data.getMemoryUsed() == 0.0) {
+                matrixNode.putNull("Memory_Used");
+            } else {
+                matrixNode.put("Memory_Used", String.format("%.3f", data.getMemoryUsed() / Math.pow(1024, 2)));
+            }
+
             matrixNode.put("Status", data.getStatus());
         }
 
